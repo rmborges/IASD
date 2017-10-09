b51a590688003386817ccbf9ec86d34208cf6c18
joaocpc <joaocarloscanas@gmail.com>
2017-10-09 12:49:01 +0100

    couves

--- read_data.py
+++ read_data.py
@@ -17,14 +17,38 @@ class Edge(object):
         self.vertex1 = vertex1
         self.vertex2 = vertex2
 
-    def print_vertex(self):
+    def print_edge(self):
         print('edge', self.vertex1, self.vertex2)
+
+class Launch(object):
+    date= ""
+    max_payload = 0
+    fixed_cost = 0
+    variable_cost = 0
+
+    def __init__(self, date, max_payload, fixed_cost, variable_cost):
+        self.date = date
+        self.max_payload = max_payload
+        self.fixed_cost = fixed_cost
+        self.variable_cost = variable_cost
+
+    def print_launch(self):
+        print('launch', self.date, self.max_payload, self.fixed_cost, self.variable_cost)
 
 # leitura do ficheiro com os dados
 iss_data = open('iss.txt', 'r')
 
 v = 0
 vertex_list = []
+
+e = 0
+edge_list = []
+
+l = 0
+launch_list = []
+
+
+
 while True:
     line = iss_data.readline()
 
@@ -40,20 +64,20 @@ while True:
         vertex_list[v].print_vertex()
         v = v + 1
 
+    if line[0] == 'E':
+        vertex1 = fields[1]
+        vertex2 = fields[2]
+        edge_list.append(Edge(vertex1, vertex2))
+        edge_list[e].print_edge()
+        e = e + 1
 
-v = 0
-vertex_list = []
-while True:
-    line = iss_data.readline()
+    if line[0] == 'L':
+        date = fields[1]
+        max_payload = fields[2]
+        fixed_cost = fields[3]
+        variable_cost = fields[4]
 
-    if not line:
-        break
+        launch_list.append(Launch(date, max_payload, fixed_cost, variable_cost))
+        launch_list[l].print_launch()
+        l = l + 1
 
-    fields = line.split()
-
-    if line[0] == 'V':
-        id = fields[0]
-        weight = float(fields[1])
-        vertex_list.append(Vertex(id, weight))
-        vertex_list[v].print_vertex()
-        v = v + 1
