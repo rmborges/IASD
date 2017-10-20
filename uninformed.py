c2294649cd32e04a1c72d049915cfc0cbac2ab5c
rmborges <rafael.m.borges@tecnico.ulisboa.pt>
2017-10-20 17:30:52 +0100

    uninformed functions

---
+++ uninformed.py
@@ -0,0 +1,15 @@
+# funções dependentes do problema
+
+def successorFunc():
+
+
+def strategyFunc(open_list):
+    return min(open_list, key=lambda node: node.cost)
+
+def goalCheck():
+
+
+def gFunc(node):
+
+
+    return cost
